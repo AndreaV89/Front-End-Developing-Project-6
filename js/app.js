709be5b9afe116c68bb50b3b12e0188005f624d3
AndreaV <a.vannetti08@gmail.com>
2019-12-27 12:33:06 +0100

    mmm

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function getRandomPhraseAsArray (arr) {
     const characters = [];
     const index = Math.floor(Math.random() * arr.length);
     for (let i = 0; i < phrases[index].length; i++) {
-        let  char = phrases[index].charAt(i);
+        let  char = phrases[index].charAt(i).toLowerCase();
         characters.push(char);
     }
     return characters;
@@ -30,19 +30,37 @@ function addPhraseToDisplay (arr) {
         ul.appendChild(li);
         if (li.textContent !== ' ') {
             li.className = 'letter'; 
+        } else {
+            li.className = 'space';
         }
     }
 }
 
-function checkLetter () {
-
+function checkLetter (button) {
+    const letters = ul.getElementsByTagName('li');
+    for (let i = 0; i < letters.length ; i++) {
+        if (button.textContent === letters[i].textContent) {
+            letters[i].className += ' ' + 'show';
+            letter = letters[i].textContent;
+            return letter;
+        }
+    }
 }
-
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
 
 // Hide overlay
 startButton[0].addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
+qwertyDiv.addEventListener('click', function(e) {
+    e.target.className = 'chosen';
+    e.target.setAttribute ('disabled', '');
+    checkLetter(e.target);    
+})
+
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+
