25a6376be945201379cd5262d87b630658e99a32
AndreaV <a.vannetti08@gmail.com>
2019-12-28 16:46:59 +0100

    mmm

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ const qwertyDiv = document.getElementById('qwerty');
 const phraseDiv = document.getElementById('phrase');
 const startButton = document.getElementsByClassName('btn__reset');
 const ul = document.querySelector('#phrase ul');
-let missed = 0;
+const tries = document.querySelectorAll('.tries img');
+const overlay = document.getElementById('overlay');
+var missed = 0;
+var letterFound;
 const phrases = [
     'Java Script', 
     'Pyt hon', 
@@ -10,10 +13,10 @@ const phrases = [
     'Sw ift', 
     'Ru by'
 ];
+const characters = [];
 
 // return random phrase from the array
 function getRandomPhraseAsArray (arr) {
-    const characters = [];
     const index = Math.floor(Math.random() * arr.length);
     for (let i = 0; i < phrases[index].length; i++) {
         let  char = phrases[index].charAt(i).toLowerCase();
@@ -22,7 +25,7 @@ function getRandomPhraseAsArray (arr) {
     return characters;
 }
 
-
+// display the random phrase
 function addPhraseToDisplay (arr) {
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement('LI');
@@ -36,31 +39,65 @@ function addPhraseToDisplay (arr) {
     }
 }
 
+// check if the chosen letter is in the phrase or not
 function checkLetter (button) {
     const letters = ul.getElementsByTagName('li');
+    var check = false;
     for (let i = 0; i < letters.length ; i++) {
         if (button.textContent === letters[i].textContent) {
             letters[i].className += ' ' + 'show';
-            letter = letters[i].textContent;
-            return letter;
+            letterFound = letters[i].textContent;
+            check = true;
         }
+    }
+    if (check === false) {
+        letterFound = null;
     }
 }
 
-// Hide overlay
+// restart game
+function restartGame() {
+    const li = ul.getElementsByTagName('li');
+    while (li.length > 0) {
+        ul.removeChild(li[0]);
+    }
+}
+
+// check win or lose
+function checkWin () {
+    const letters = ul.getElementsByClassName('letter');
+    const letterShow = ul.getElementsByClassName('letter show');
+    if (letterShow.length === letters.length) {
+        overlay.setAttribute('class', 'win');
+        overlay.style.display = 'flex';
+        restartGame();
+    } else if (missed >= 5){
+        overlay.setAttribute('class', 'lose');
+        overlay.style.display = 'flex';
+        restartGame();
+    }
+
+}
+
+// Hide overlay and start game
 startButton[0].addEventListener('click', () => {
-    const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
 });
 
 qwertyDiv.addEventListener('click', function(e) {
     e.target.className = 'chosen';
     e.target.setAttribute ('disabled', '');
-    checkLetter(e.target);    
+    checkLetter(e.target); 
+    if (letterFound === null) {
+        tries[missed].setAttribute ('src', 'images/lostHeart.png');
+        missed ++;
+    } 
+    checkWin();
 })
 
 
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
+
 
 
